0711b4d5c752458b7f374d34c1703c9214bb1835
olyphotographer <olyphotographer@gmail.com>
2020-03-05 09:58:39 -0500

    3

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -12,9 +12,27 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    let ballArray = [#imageLiteral(resourceName: "ball1"),#imageLiteral(resourceName: "ball2"),#imageLiteral(resourceName: "ball3"),#imageLiteral(resourceName: "ball4"),#imageLiteral(resourceName: "ball5")]
+    
+    var letzteZahl : Int = 99
 
 
-
+    
+    
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        
+        var zufall = Int.random(in: 0...4)
+        while letzteZahl == zufall {
+           zufall = Int.random(in: 0...4)
+        }
+        print(letzteZahl, zufall)
+        letzteZahl = zufall
+        
+        imageView.image = ballArray[zufall]
+       
+    }
 }
 
